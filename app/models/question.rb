3aa542f2d60ab3ee1a5110a01614a632d6390ea6
Ria I <ddmlbix@gmail.com>
2023-01-02 23:43:09 +0300

    Security fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ApplicationRecord
   belongs_to :user
 
   validates :body,
-            format: { without: /<.+>/ },
             length: { maximum: 280 },
             presence: true
 end
