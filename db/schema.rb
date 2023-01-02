d24ef4516404d5b30e4e7443d2bca7516bf77c84
Ria I <ddmlbix@gmail.com>
2023-01-02 13:38:14 +0300

    Added unique email index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_01_214933) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_103716) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_01_214933) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "navbar_color", default: "nil"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
